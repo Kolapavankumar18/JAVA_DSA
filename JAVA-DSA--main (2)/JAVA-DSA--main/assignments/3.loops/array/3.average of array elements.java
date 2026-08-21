d755ca51a99bd20025357b9121fbef75ef1ecced
Pavankumarkola18 <pavankumarkola18@gmail.com>
2026-08-21 15:25:18 +0530

    Delete JAVA-DSA--main (2)/JAVA-DSA--main/assignments directory

--- JAVA-DSA--main (2)/JAVA-DSA--main/assignments/3.loops/array/3.average of array elements.java
+++
@@ -1,8 +0,0 @@
-public class AverageArray {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int sum = 0;
-        for (int x : arr) sum += x;
-        System.out.println("Average = " + (double)sum / arr.length);
-    }
-}
--- JAVA-DSA--main (2)/JAVA-DSA--main/assignments/3.loops/matrix/3.largest in matrix.java
+++
@@ -1,7 +0,0 @@
-public class LargestMatrix {
-    public static void main(String[] args) {
-        int[][] mat = {{1,2},{3,4}};
-        int max = mat[0][0]; for (int[] row : mat) for (int x : row) if (x > max) max = x;
-        System.out.println(max);
-    }
-}
